2a872f0b384a60fb6801ba2d93b255422c42ba87
Anuj Balmiki <137060910+Anujkbalmiki@users.noreply.github.com>
2024-04-26 14:29:23 +0530

    Update contact-manager.component.ts

--- src/app/contact-manager/contact-manager.component.ts
+++ src/app/contact-manager/contact-manager.component.ts
@@ -7,18 +7,18 @@ import { ContactService } from '../services/contact.service';
   templateUrl: './contact-manager.component.html',
   styleUrl: './contact-manager.component.css'
 })
-export class ContactManagerComponent implements OnInit{
-  public loading:boolean = false;
-  public contacts:myContact[] = [];
-  public errorMessage:string | null = null;
+export class ContactManagerComponent{}
+  // public loading:boolean = false;
+  // public contacts:myContact[] = [];
+  // public errorMessage:string | null = null;
 
 
-  constructor(private cantService:ContactService){
+  // constructor(private cantService:ContactService){
 
-  }
-  ngOnInit(): void{
-    this.loading=true;
-    this.cantService.getAllContacts().subscribe((data:myContact[])=>{
-      this.contacts;
-    })
-  }
+  // }
+  // ngOnInit(): void{
+  //   this.loading=true;
+  //   this.cantService.getAllContacts().subscribe((data:myContact[])=>{
+  //     this.contacts;
+  //   })
+  // }
